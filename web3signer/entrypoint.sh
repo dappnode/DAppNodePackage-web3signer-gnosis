bf180f132133b7e75d2df8fa6a419043e0e7356d
pablo <mendez4a@gmail.com>
2022-03-15 09:27:40 +0100

    set hostname

--- web3signer/entrypoint.sh
+++ web3signer/entrypoint.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
-# Check ens exist
+# Check envs exist
 [ -z "$KEYFILES_DIR" ] && echo "KEYFILES_DIR env is not set" && exit 1
 [ -z "$SIGNER_PORT" ] && echo "SIGNER_PORT env is not set" && exit 1
+[ -z "$ETH2_CLIENT" ] && echo "SIGNER_PORT env is not set" && exit 1
+
+# Assign proper value to ETH2_CLIENT
+case "$ETH2_CLIENT" in
+  "prysm")
+    ETH2_CLIENT="validator.prysm-prater-dappnode"
+    ;;
+  "teku")
+    ETH2_CLIENT="teku-prater.dappnode" # !IMPORTANT: there is only one container in teku
+    ;;
+  "lighthouse")
+    ETH2_CLIENT="validator.lighthouse-prater-dappnode"
+    ;;
+  "nimbus")
+    ETH2_CLIENT="validator.nimbus-prater-dappnode"
+    ;;
+  "all")
+    ETH2_CLIENT="*"
+    ;;
+  *)
+    echo "ETH2_CLIENT env is not set propertly" && exit 1
+    ;;
+esac
     
 # IMPORTANT! The dir defined for --key-store-path must exist and have specific permissions. Should not be created with a docker volume
 mkdir -p "$KEYFILES_DIR"
@@ -13,7 +36,7 @@ exec /opt/web3signer/bin/web3signer \
     --key-store-path="$KEYFILES_DIR" \
     --http-listen-port="$SIGNER_PORT" \
     --http-listen-host=0.0.0.0 \
-    --http-host-allowlist=* \
+    --http-host-allowlist="$ETH2_CLIENT" \
     --http-cors-origins=* \
     eth2 \
     --network=prater \
