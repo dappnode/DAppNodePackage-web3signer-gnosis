c0473fcd3e51df66a1419936137522f8a7db4c1e
Pablo <mendez4a@gmail.com>
2022-04-29 17:05:11 +0200

    implement load keys on the fly

--- web3signer/entrypoint.sh
+++ web3signer/entrypoint.sh
@@ -1,52 +1,82 @@
 #!/bin/bash
 
-# Check envs exist
-[ -z "$KEYFILES_DIR" ] && echo "KEYFILES_DIR env is not set" && exit 1
-[ -z "$SIGNER_PORT" ] && echo "SIGNER_PORT env is not set" && exit 1
-[ -z "$ETH2_CLIENT" ] && echo "ETH2_CLIENT env is not set" && exit 1
+SIGNER_PORT=9000
+KEYFILES_DIR="/opt/web3signer/keyfiles"
+export NETWORK="prater"
+export WEB3SIGNER_API="http://web3signer.web3signer-${NETWORK}.dappnode:${SIGNER_PORT}"
 
 # Assign proper value to ETH2_CLIENT. The UI uses the web3signer domain in the Header "Host"
 case "$ETH2_CLIENT" in
-  "prysm")
-    ETH2_CLIENT="web3signer.web3signer-prater.dappnode,validator.prysm-prater.dappnode"
-    ;;
-  "teku")
-    ETH2_CLIENT="web3signer.web3signer-prater.dappnode,validator.teku-prater.dappnode"
-    ;;
-  "lighthouse")
-    ETH2_CLIENT="web3signer.web3signer-prater.dappnode,validator.lighthouse-prater.dappnode"
-    ;;
-  "nimbus")
-    ETH2_CLIENT="web3signer.web3signer-prater.dappnode,validator.nimbus-prater.dappnode"
-    ;;
-  "all")
-    ETH2_CLIENT="*"
-    ;;
-  *)
-    echo "ETH2_CLIENT env is not set propertly"
-    ETH2_CLIENT="none"
-    ;;
+"prysm")
+  ETH2_CLIENT_DNS="validator.prysm-prater.dappnode"
+  export BEACON_NODE_API="http://beacon-chain.prysm-prater.dappnode:3500"
+  export CLIENT_API="http://validator.prysm-prater.dappnode:3500"
+  export "${TOKEN_FILE}"="/security/prysm/auth-token"
+  ;;
+"teku")
+  ETH2_CLIENT_DNS="validator.teku-prater.dappnode"
+  export BEACON_NODE_API="http://beacon-chain.teku-prater.dappnode:3500"
+  export CLIENT_API="https://validator.teku-prater.dappnode:3500"
+  export "${TOKEN_FILE}"="/security/teku/validator-api-bearer"
+  ;;
+"lighthouse")
+  ETH2_CLIENT_DNS="validator.lighthouse-prater.dappnode"
+  export BEACON_NODE_API="http://beacon-chain.lighthouse-prater.dappnode:3500"
+  export CLIENT_API="http://validator.lighthouse-prater.dappnode:3500"
+  export "${TOKEN_FILE}"="/security/lighthouse/auth-token"
+  ;;
+"nimbus")
+  ETH2_CLIENT_DNS="beacon-validator.nimbus-prater.dappnode"
+  export BEACON_NODE_API="http://beacon-validator.nimbus-prater.dappnode:4500"
+  export CLIENT_API="http://beacon-validator.nimbus-prater.dappnode:3500"
+  export "${TOKEN_FILE}"="/security/nimbus/auth-token"
+  ;;
+"all")
+  ETH2_CLIENT_DNS="*"
+  ;;
+*)
+  echo "ETH2_CLIENT env is not set propertly"
+  exit 1
+  ;;
 esac
-    
+
+if [[ $LOG_TYPE == "DEBUG" ]]; then
+  export LOG_LEVEL=0
+elif [[ $LOG_TYPE == "INFO" ]]; then
+  export LOG_LEVEL=1
+elif [[ $LOG_TYPE == "WARN" ]]; then
+  export LOG_LEVEL=2
+elif [[ $LOG_TYPE == "ERROR" ]]; then
+  export LOG_LEVEL=3
+else
+  export LOG_LEVEL=1
+fi
+
 # IMPORTANT! The dir defined for --key-store-path must exist and have specific permissions. Should not be created with a docker volume
 mkdir -p "$KEYFILES_DIR"
 
+# Loads envs into /etc/environment to be used by the cronjob
+env >>/etc/environment
+# start cron and disown it
+cron -f &
+disown
+
 # Run web3signer binary
 # - Run key manager (it may change in the future): --key-manager-api-enabled=true
-exec /opt/web3signer/bin/web3signer \
-    --key-store-path="$KEYFILES_DIR" \
-    --http-listen-port="$SIGNER_PORT" \
-    --http-listen-host=0.0.0.0 \
-    --http-host-allowlist="$ETH2_CLIENT" \
-    --http-cors-origins=* \
-    --metrics-enabled=true \
-    --metrics-host 0.0.0.0 \
-    --metrics-port 9091 \
-    --metrics-host-allowlist="*" \
-    eth2 \
-    --network=prater \
-    --slashing-protection-db-url=jdbc:postgresql://postgres:5432/web3signer \
-    --slashing-protection-db-username=postgres \
-    --slashing-protection-db-password=password \
-    --key-manager-api-enabled=true \
-    ${EXTRA_OPTS}
+exec -c /opt/web3signer/bin/web3signer \
+  --key-store-path="$KEYFILES_DIR" \
+  --http-listen-port="$SIGNER_PORT" \
+  --http-listen-host=0.0.0.0 \
+  --http-host-allowlist="web3signer.web3signer-prater.dappnode,$ETH2_CLIENT_DNS" \
+  --http-cors-origins=* \
+  --metrics-enabled=true \
+  --metrics-host 0.0.0.0 \
+  --metrics-port 9091 \
+  --metrics-host-allowlist="*" \
+  eth2 \
+  --network=prater \
+  --slashing-protection-db-url=jdbc:postgresql://postgres:5432/web3signer \
+  --slashing-protection-db-username=postgres \
+  --slashing-protection-db-password=password \
+  --key-manager-api-enabled=true \
+  ${EXTRA_OPTS}
