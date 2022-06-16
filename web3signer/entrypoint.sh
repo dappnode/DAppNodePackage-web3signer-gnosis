24c095fda43b120a039342b45a819600ea5f1871
pablomendezroyo <41727368+pablomendezroyo@users.noreply.github.com>
2022-06-16 15:13:41 +0200

    update postgres dns and db password

--- web3signer/entrypoint.sh
+++ web3signer/entrypoint.sh
@@ -79,8 +79,8 @@ exec /opt/web3signer/bin/web3signer \
   --metrics-host-allowlist="*" \
   eth2 \
   --network=gnosis \
-  --slashing-protection-db-url=jdbc:postgresql://postgres:5432/web3signer \
+  --slashing-protection-db-url=jdbc:postgresql://postgres.web3signer-gnosis.dappnode:5432/web3signer \
   --slashing-protection-db-username=postgres \
-  --slashing-protection-db-password=password \
+  --slashing-protection-db-password=gnosis \
   --key-manager-api-enabled=true \
   ${EXTRA_OPTS}
