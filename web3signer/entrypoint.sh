178197f94bd210a671efcf3d973f34c71357c46e
dappnodedev <diego@dappnode.io>
2023-11-22 11:35:59 +0100

    Reduce w3s size

--- web3signer/entrypoint.sh
+++ web3signer/entrypoint.sh
@@ -44,7 +44,7 @@ esac
 mkdir -p "$KEYFILES_DIR"
 mkdir -p "/opt/web3signer/manual_migration"
 
-if grep -Fq "/opt/web3signer/keyfiles" ${KEYFILES_DIR}/*.yaml ;then
+if grep -Fq "/opt/web3signer/keyfiles" ${KEYFILES_DIR}/*.yaml; then
   sed -i "s|/opt/web3signer/keyfiles|$KEYFILES_DIR|g" ${KEYFILES_DIR}/*.yaml
 fi
 
@@ -64,9 +64,9 @@ exec /opt/web3signer/bin/web3signer \
   eth2 \
   --network=gnosis \
   --Xnetwork-capella-fork-epoch=648704 \
-  --slashing-protection-db-url=jdbc:postgresql://postgres.web3signer-gnosis.dappnode:5432/web3signer-gnosis \
+  --slashing-protection-db-url=jdbc:postgresql://postgres.web3signer-gnosis.dappnode:5432/web3signer \
   --slashing-protection-db-username=postgres \
-  --slashing-protection-db-password=gnosis \
+  --slashing-protection-db-password=password \
   --slashing-protection-pruning-enabled=true \
   --slashing-protection-pruning-epochs-to-keep=500 \
   --key-manager-api-enabled=true \
